3d1d5838b97d3f5f98fd9ae80a6228056b316f45
Fudanyrd <143480204+Fudanyrd@users.noreply.github.com>
2023-09-15 17:53:16 +0800

    Add Tuple.java

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ File.java:          How to read from and print to a file.
 Parrot.java:        How to read/write to Standard Input/Output stream.    
 Enum.java:          How to use variable with type *enum*    
 JMath.java:         How to use (static) java.lang.Math package.
+Tuple.java:         How to create class, abstract class and syntax about inherence.
 
 #Hopefully these examples will help you learn the most important things in java!     
 #Have Fun!    
---
+++ Tuple.java
@@ -0,0 +1,52 @@
+
+public class Tuple {
+    public static abstract class tuple{
+        abstract void output();
+    }; 
+
+    public static class dubTuple extends tuple{
+        public int x;
+        public int y;
+
+        dubTuple(int x, int y){
+            this.x = x; this.y = y;
+        }
+        dubTuple(){
+            this.x = this.y = 0;
+        }
+        void output() {
+            System.out.printf("(%d, %d)", this.x,this.y);
+        }
+
+    }
+
+    public static class triTuple extends dubTuple{
+        public int z;
+
+        triTuple(int x,int y,int z){
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        triTuple(){
+            this(0,0,0);
+        }
+
+        void output(){
+            System.out.printf("(%d, %d, %d)",this.x,this.y,this.z);
+        }
+    }
+
+    public static void main(String[] argv){
+        dubTuple a = new Tuple.dubTuple(23,44);
+        System.out.printf("a = ");
+        a.output();
+
+        triTuple b = new Tuple.triTuple(-12, 53, -88);
+        System.out.printf("\nb = ");
+        b.output();
+
+        return; 
+
+    }
+}
